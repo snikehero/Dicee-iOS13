2960b39fd50d358ca99352952a84667fadafacdc
snikehero <snikehero234@gmail.com>
2024-03-21 18:10:01 -0600

    commit
    Commit following course

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageView1: UIView!
+
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    let diceArray: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //Can use force unwrap because we are working with an array with 5 concrete elements, so randomElement will always be kept.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: diceArray.randomElement()!)
+        
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: diceArray[Int.random(in: 0...5)])
+        
+    }
 }
 
